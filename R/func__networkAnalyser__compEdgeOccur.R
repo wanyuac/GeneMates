2e0f074d9bab630e2705b09811df58fc3fc2db68
Yu Wan <wanyuac@126.com>
2018-10-28 20:45:53 +1100

    Upgrade function summariseCliques

--- R/func__networkAnalyser__compEdgeOccur.R
+++ R/func__networkAnalyser__compEdgeOccur.R
@@ -3,16 +3,16 @@
 #' @description Compute a matrix of Simpson's similarity coefficients for the
 #' presence of edges.
 #'
-#' @param nwk A Graph object created by the function mkNetwork for a single network.
+#' @param net A Graph object created by the function mkNetwork for a single network.
 #' It has three attributes: id, E and V. Required columns of E are "y" and "x",
 #' which are vertex names.
 #' @param apam Allelic presence-absence matrix. Rows: strain names; columns:
 #' allele names.
-#' @param directed A logical argument specifying whether nwk stores a directed network.
+#' @param directed A logical argument specifying whether net stores a directed network.
 #' Default: TRUE.
 #'
-#' @return A list of three elements: nwk, an undirected network converted from
-#' the input network; ep, a data frame of edge pairs; sim, the similarity matrix
+#' @return A list of three elements: net, an undirected network converted from
+#' the input network; es, a data frame of edge pairs; sim, the similarity matrix
 #' for edges, which is a symmetric matrix.
 #'
 #' @examples compEdgeOccur
@@ -22,37 +22,37 @@
 #'
 #  Copyright 2018 Yu Wan
 #  Licensed under the Apache License, Version 2.0
-#  First version: 6 Aug 2018; the latest edition: 11 Aug 2018
+#  First version: 6 Aug 2018; the latest edition: 28 Oct 2018
 
-compEdgeOccur <- function(nwk, apam, directed = TRUE) {
-    # Convert nwk into an undirected network
+compEdgeOccur <- function(net, apam, directed = TRUE) {
+    # Convert net into an undirected network
     if (directed) {  # Vertices will not change in this process.
-        if (!any(names(nwk@E) == "pair")) {
-            nwk@E <- assignPairID(lmms = nwk@E, from = 1, paired.rows = FALSE)  # A "pair" column is appended.
+        if (!any(names(net@E) == "pair")) {
+            net@E <- assignPairID(lmms = net@E, from = 1, paired.rows = FALSE)  # A "pair" column is appended.
         }
-        nwk@E <- nwk@E[!duplicated(nwk@E$pair), ]  # only preserve one edge per pair of edges
+        net@E <- net@E[!duplicated(net@E$pair), ]  # only preserve one edge per pair of edges
     }
 
     # Prepare the data frame of edge pairs
-    ep <- nwk@E[, c("pair", "y", "x")]
-    ep <- ep[order(ep$pair, decreasing = FALSE), ]
-    ep$pair <- paste0("e", ep$pair)  # 1, 2, ... => e1, e2, ...
-    names(ep)[1] <- "edge_ID"  # replace the first column name
-    ep$edge_name <- mapply(function(a1, a2) paste(a1, a2, sep = "&"), ep$y, ep$x)
-    eids <- ep$edge_ID
+    es <- net@E[, c("pair", "y", "x")]  # only keep three columns for edges
+    es <- es[order(es$pair, decreasing = FALSE), ]
+    names(es)[1] <- "edge_ID"  # replace the first column name "pair" as "edge_ID"
+    es$edge_ID <- paste0("e", es$edge_ID)  # 1, 2, ... => e1, e2, ...
+    es$edge_name <- mapply(function(a1, a2) paste(a1, a2, sep = "&"), es$y, es$x)
+    eids <- es$edge_ID
     n <- length(eids)  # number of undirected edges
 
     # Calculate a Simpson similarity for each pair of edges
     sim <- matrix(1.0, nrow = n, ncol = n, dimnames = list(eids, eids))
     for (i in 1 : (n - 1)) {
-        ei <- ep[i, ]
+        ei <- es[i, ]
         ei_id <- ei$edge_ID
         ei_x <- ei$x
         ei_y <- ei$y
         co_i <- as.logical(apam[, ei_x]) & as.logical(apam[, ei_y])  # co-occurrence of x, y alleles of the i-th edge
         ei_c <- sum(co_i)  # occurrence count of the i-th edge
         for (j in (i + 1) : n) {
-            ej <- ep[j, ]
+            ej <- es[j, ]
             ej_id <- ej$edge_ID
             ej_x <- ej$x
             ej_y <- ej$y
@@ -64,5 +64,5 @@ compEdgeOccur <- function(nwk, apam, directed = TRUE) {
         }
     }
 
-    return(list(nwk = nwk, ep = ep, sim = sim))
+    return(list(net = net, es = es, sim = sim))
 }
--- R/func__networkAnalyser__summariseCliques.R
+++ R/func__networkAnalyser__summariseCliques.R
@@ -4,6 +4,7 @@
 #' summary data frame.
 #'
 #' @param q A list produced by the function max_cliques of the package igraph.
+#' @param s An optional list produced by the function compEdgeOccur for edge similarities.
 #'
 #' @return A data frame of three columns: Clique, Size and Alleles.
 #'
@@ -12,24 +13,49 @@
 #'
 #  Copyright 2018 Yu Wan
 #  Licensed under the Apache License, Version 2.0
-#  First and the latest edition: 27 Oct 2018
+#  First and the latest edition: 28 Oct 2018
 
-summariseCliques <- function(q) {
+summariseCliques <- function(q, s = NULL) {
     require(data.table)
 
-    d <- as.data.frame(rbindlist(lapply(q, .makeLines)))
+    d <- as.data.frame(rbindlist(lapply(q, .makeLines, sim = s)))
     d <- d[order(d$Size, decreasing = TRUE), ]  # By default, the cliques functions orders cliques in an ascending order of clique sizes.
-    d$Clique <- 1 : nrow(d)
-    d <- d[, c("Clique", "Size", "Alleles")]
+    d <- cbind.data.frame(Clique = 1 : nrow(d), d)  # assign arbitrary, continuous clique IDs
 
     return(d)
 }
 
-.makeLines <- function(i) {
-    # a subordinate function of summariseCliques
+.makeLines <- function(i, sim = NULL) {  # A subordinate function of summariseCliques
+    # Basic summary
     alleles <- i$name
-    d <- data.frame(Alleles = paste(alleles, collapse = ","), Size = length(alleles),
+    d <- data.frame(Size = length(alleles), Alleles = paste(alleles, collapse = ","),
                     stringsAsFactors = FALSE)
 
-    return(d)
+    # Summarise Edge similarities
+    if (is.list(sim)) {  # assuming elements "ep" and "sim" are included
+        es <- subset(sim[["es"]], y %in% alleles & x %in% alleles) # extract undirected, non-duplicated edges for the current clique
+        eids <- es$edge_ID
+        eid_n <- length(eids)
+        if (eid_n > 2) {  # eid_n >= 3 when length(alleles) >= 3.
+            sim_mat <- sim[["sim"]][eids, eids]  # extract the relevant similarity matrix
+            sim_coes <- numeric(0)
+            for (i in 1 : (eid_n - 1)) {  # sim_coe has a minimum row number of three.
+                for (j in (i + 1) : eid_n) {
+                    sim_coes <- append(sim_coes, sim_mat[eids[i], eids[j]])
+                }
+            }
+            # Actually, for any Uni_edge > 0, S_num = Uni_edge * (Uni_edge - 1) / 2.
+            # Similarly, Uni_edges = Alleles * (Alleles - 1) / 2.
+            # However, I put Uni_edges and S_num here for users to check their outputs.
+            d <- cbind.data.frame(d, data.frame(Uni_edges = eid_n, S_num = length(sim_coes),
+                                                S_min = min(sim_coes), S_med = median(sim_coes),
+                                                S_max = max(sim_coes), stringsAsFactors = FALSE))  # Uni_edges: number of unidirected edges
+        } else {  # eid_n = 1 when length(alleles) == 2, for which no edge similarity coefficient is applicable.
+            d <- cbind.data.frame(d, data.frame(Uni_edges = eid_n, S_num = 0,
+                                                S_min = NA, S_med = NA, S_max = NA,
+                                                stringsAsFactors = FALSE))
+        }
+    }
+
+    return(d)  # returns a single-line data frame
 }
